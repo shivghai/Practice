9fe121d0ba86c2ded49a373db21c3081ebe7d0cc
shivghai <sghai@umich.edu>
2015-09-29 23:12:14 -0400

    Fixed - saw after review that it didn't work

--- alphacode.cpp
+++ alphacode.cpp
@@ -3,27 +3,40 @@
 #include <vector>
 using namespace std;
 
-
 int main() {
 	string str;
+	long long DP[5000];
 	cin >> str;
-	vector<int> vec(str.size() + 1, 0);
-	int N = str.size() + 1;
-
-	vec[0] = 1;
-	vec[1] = 1;
-
-	for(int i = 2; i <= N; ++i) {
-		if(str[i - 1] == '1' || (str[i - 1] == '2' && 
-			str[i] <= '6') ) {
-			vec[i] = vec[i - 1] + vec[i - 2];
+	while(str[0] != '0') {
+	
+		for(int i = 0; i < 5000; ++i) {
+			DP[i] = 0;
 		}
 
-		else {
-			vec[i] = vec[i - 1];
-			
+		str = "X" + str;
+		int N = str.size();
+	
+		DP[0] = 1;
+		DP[1] = 1;
+		for(int i = 2; i < N; ++i) {
+			if(str[i - 1] == '1' && str[i] != '0') {
+				if((i + 1 < N && str[i + 1] != '0') || i + 1 >= N)
+					DP[i] = DP[i - 1] + DP[i - 2];
+				else
+					DP[i] = DP[i - 1];
+			}
+			else if(str[i - 1] == '2' && str[i] <= '6' && str[i] != '0') {
+				if((i + 1 < N && str[i + 1] != '0') || i + 1 >= N)
+					DP[i] = DP[i - 1] + DP[i - 2];
+				else
+					DP[i] = DP[i - 1];
+			}
+			else
+				DP[i] = DP[i - 1];
 		}
+		cout << DP[N - 1] << "\n";
+		cin >> str;
 	}
-	cout << vec.back() << endl;
+	
 	return 0;
 }
